8d23752e09ee5a32b8e3010986a7bd54201ed2ee
Tales Cione <talesj@gmail.com>
2025-08-16 10:43:19 +0200

    feat: fetch sun data for berlin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+fn fetch_sunrise_sunset() -> Result<String, Box<dyn std::error::Error>> {
+    let berlin_lat = "52.56";
+    let berlin_lon = "13.39";
+    let url = format!(
+        "https://api.sunrisesunset.io/json?lat={}&lng={}&time_format=military",
+        berlin_lat, berlin_lon
+    );
+    let response = reqwest::blocking::get(&url)?;
+    let text = response.text()?;
+    Ok(text)
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Sundial starting...");
+
+    match fetch_sunrise_sunset() {
+        Ok(response) => println!("API Response: {}", response),
+        Err(e) => println!("Error: {}", e),
+    }
+    // TODO: fetch sunrise/sunset data
+    // TODO: get current time
+    // TODO: determine day/night
+    // TODO: check process status
+    // TODO: update temperature if needed
 }
