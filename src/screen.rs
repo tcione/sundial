fb898b42d37972b7dbd85b9b495628cbf914d7d7
Tales Cione <talesj@gmail.com>
2025-08-24 22:59:40 +0200

    feat: make fade more granular
    Defining steps felt too arbritary, more complex and sort of weird
    construct. I.e, how the hell does one decide how many steps?
    
    I think having a convention that each minute is a step sets us off for a
    better experience:
    
    - Users can still choose "steps" based on how often they run sundial
    - Minutes feel more natural (you can get a smooth transition instead of
      choppy bits)

--- src/screen.rs
+++ src/screen.rs
@@ -10,8 +10,7 @@ pub struct ScreenState {
 
 fn calculate_fade_state(is_day: bool, target_time: NaiveTime, sun_times: &SunTimes, config: &Config) -> ScreenState {
     let fade_duration = Duration::minutes(config.screen.fade_duration_in_minutes);
-    let fade_steps = config.screen.fade_steps;
-    let step_duration = fade_duration.num_minutes() / fade_steps;
+    let fade_steps = fade_duration.num_minutes();
 
     let temperature_day = config.screen.day_temperature.parse::<i64>().unwrap();
     let temperature_night = config.screen.night_temperature.parse::<i64>().unwrap();
@@ -21,21 +20,25 @@ fn calculate_fade_state(is_day: bool, target_time: NaiveTime, sun_times: &SunTim
     let gamma_day = config.screen.day_gamma.parse::<i64>().unwrap();
     let gamma_night = config.screen.night_gamma.parse::<i64>().unwrap();
     let gamma_delta = (gamma_day - gamma_night).abs();
-    let gamma_step = gamma_delta / fade_steps;
+
+    // Gamma is too small of a range (i.e from 80 to 100), so when we progress
+    // by the minute, chances are the step lands in 0. To circumvent that, I've
+    // decided to convert it to float
+    let gamma_step_unrounded = gamma_delta as f64 / fade_steps as f64;
+    let gamma_step = format!("{:.2}", gamma_step_unrounded).parse::<f64>().unwrap();
 
     let diff_base = if is_day { sun_times.sunset } else { sun_times.sunrise };
     let difference_in_mins = (diff_base - target_time).num_minutes();
-    let current_step = difference_in_mins / step_duration;
-    let factor  = fade_steps - current_step;
+    let factor  = fade_steps - difference_in_mins;
 
     let temperature;
     let gamma;
     if is_day {
         temperature = temperature_day - (temperature_step * factor);
-        gamma = gamma_day - (gamma_step * factor);
+        gamma = gamma_day as f64 - (gamma_step * factor as f64);
     } else {
         temperature = temperature_night + (temperature_step * factor);
-        gamma = gamma_night + (gamma_step * factor);
+        gamma = gamma_night as f64 + (gamma_step * factor as f64);
     }
 
     ScreenState { temperature: temperature.to_string(), gamma: gamma.to_string() }
@@ -90,37 +93,25 @@ mod tests {
                 NaiveTime::from_hms_opt(05, 00, 00).unwrap(),
                 config.screen.night_temperature.clone(),
                 config.screen.night_gamma.clone(),
-                "Dawn fade step 1"
+                "Dawn fade at 0 mins"
             ),
             (
-                NaiveTime::from_hms_opt(05, 10, 00).unwrap(),
-                "3333".to_string(),
-                "83".to_string(),
-                "Dawn fade step 2"
-            ),
-            (
-                NaiveTime::from_hms_opt(05, 20, 00).unwrap(),
-                "3866".to_string(),
-                "86".to_string(),
-                "Dawn fade step 3"
+                NaiveTime::from_hms_opt(05, 01, 00).unwrap(),
+                "2853".to_string(),
+                "80.33".to_string(),
+                "Dawn fade at 1 min"
             ),
             (
                 NaiveTime::from_hms_opt(05, 30, 00).unwrap(),
-                "4399".to_string(),
-                "89".to_string(),
-                "Dawn fade step 4"
+                "4390".to_string(), // 2800 + (53 * 30)
+                "89.9".to_string(), // 80.0 + (0.33 * 30)
+                "Dawn fade at 30 mins"
             ),
             (
-                NaiveTime::from_hms_opt(05, 40, 00).unwrap(),
-                "4932".to_string(),
-                "92".to_string(),
-                "Dawn fade step 5"
-            ),
-            (
-                NaiveTime::from_hms_opt(05, 50, 00).unwrap(),
-                "5465".to_string(),
-                "95".to_string(),
-                "Dawn fade step 6"
+                NaiveTime::from_hms_opt(05, 59, 00).unwrap(),
+                "5927".to_string(),
+                "99.47".to_string(),
+                "Dawn fade at last minute"
             ),
             (
                 NaiveTime::from_hms_opt(06, 00, 00).unwrap(),
@@ -138,37 +129,25 @@ mod tests {
                 NaiveTime::from_hms_opt(17, 00, 00).unwrap(),
                 config.screen.day_temperature.clone(),
                 config.screen.day_gamma.clone(),
-                "Evening fade step 1"
+                "Evening fade at 0 mins"
             ),
             (
-                NaiveTime::from_hms_opt(17, 10, 00).unwrap(),
-                "5467".to_string(),
-                "97".to_string(),
-                "Evening fade step 2"
-            ),
-            (
-                NaiveTime::from_hms_opt(17, 20, 00).unwrap(),
-                "4934".to_string(),
-                "94".to_string(),
-                "Evening fade step 3"
+                NaiveTime::from_hms_opt(17, 01, 00).unwrap(),
+                "5947".to_string(),
+                "99.67".to_string(),
+                "Evening fade at 1 min"
             ),
             (
                 NaiveTime::from_hms_opt(17, 30, 00).unwrap(),
-                "4401".to_string(),
-                "91".to_string(),
-                "Evening fade step 4"
+                "4410".to_string(), // 6000 - (53 * 30)
+                "90.1".to_string(), // 100 - (0.33 * 30)
+                "Evening fade at 30 mins"
             ),
             (
-                NaiveTime::from_hms_opt(17, 40, 00).unwrap(),
-                "3868".to_string(),
-                "88".to_string(),
-                "Evening fade step 5"
-            ),
-            (
-                NaiveTime::from_hms_opt(17, 50, 00).unwrap(),
-                "3335".to_string(),
-                "85".to_string(),
-                "Evening fade step 6"
+                NaiveTime::from_hms_opt(17, 59, 00).unwrap(),
+                "2873".to_string(),
+                "80.53".to_string(),
+                "Evening fade at last min"
             ),
             (
                 NaiveTime::from_hms_opt(18, 00, 00).unwrap(),
