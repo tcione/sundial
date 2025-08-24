eac08f48d382d8066a4420c8444f91677a1b3213
Tales Cione <talesj@gmail.com>
2025-08-24 05:48:15 +0200

    feat: basic logging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::path::PathBuf;
+use log::{info, warn, error, debug, trace};
 
 mod config;
 use config::{Config, get_config_dir, load_config};
@@ -17,11 +18,19 @@ struct Application {
     data_dir: PathBuf,
 }
 
+const NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 impl Application {
     pub fn new() -> Result<Self, Box<dyn std::error::Error>> {
+        env_logger::init();
+
         let config_dir = get_config_dir()?;
         let data_dir = get_data_dir()?;
         let config = load_config(config_dir.clone())?;
+
+        info!("Starting out {}", NAME);
+        info!("Version: {}", VERSION);
 
         Ok(Application { config, data_dir })
     }
@@ -39,12 +48,14 @@ impl Application {
             .output()?;
         let is_hyprsunset_running = hyprsunset_process.status.success();
         if is_hyprsunset_running {
+            debug!("Hyprsunset is running. Process: {:?}", hyprsunset_process);
             return Ok(());
         }
 
-        std::process::Command::new("systemctl")
+        let new_hyprsunset_process = std::process::Command::new("systemctl")
             .args(["--user", "start", "hyprsunset"])
             .output()?;
+        debug!("Starting hyprsunset. Process: {:?}", new_hyprsunset_process);
 
         Ok(())
     }
@@ -52,17 +63,29 @@ impl Application {
     fn get_sun_times(&self) -> Result<SunTimes, Box<dyn std::error::Error>> {
         Ok(
             match load_cache(&self.config, &self.data_dir) {
-                Ok(Some(cache)) => { cache.sun_times },
+                Ok(Some(cache)) => {
+                    let cached_sun_times = cache.sun_times;
+
+                    debug!("Cached sun_times in UTC: {:?}", cached_sun_times);
+
+                    cached_sun_times
+                },
                 Ok(None) => {
                     let url = build_sunrisesunset_url(&self.config);
                     let sun_times = fetch_sunrise_sunset(&url)?;
                     persist_to_cache(&self.config, &self.data_dir, &sun_times)?;
+
+                    debug!("[No cache] Fresh sun_times in UTC: {:?}", sun_times);
+
                     sun_times
                 },
                 Err(_) => {
                     let url = build_sunrisesunset_url(&self.config);
                     let sun_times = fetch_sunrise_sunset(&url)?;
                     persist_to_cache(&self.config, &self.data_dir, &sun_times)?;
+
+                    warn!("[Cache error] Fresh sun_times in UTC: {:?}", sun_times);
+
                     sun_times
                 }
             }
@@ -73,6 +96,8 @@ impl Application {
         let sun_times = self.get_sun_times()?;
         let now = chrono::Utc::now().time();
         let screen_state = calculate_screen_state(now, &sun_times, &self.config);
+
+        info!("Setting screen to: {:?}", screen_state);
 
         std::process::Command::new("hyprctl")
             .args(["hyprsunset", "temperature", &screen_state.temperature])
@@ -86,5 +111,11 @@ impl Application {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    Application::new()?.run()
+    match Application::new()?.run() {
+        Ok(()) => { Ok(()) },
+        Err(error) => {
+            error!("Error: {:?}", error);
+            panic!("ERROR!ERROR!ERROR!");
+        }
+    }
 }
