ae775028ffbec532814ecf03bcd6ae4a46aa1a54
Tales Cione <talesj@gmail.com>
2025-08-25 22:05:33 +0200

    feat: distribution via flake (basic)

--- flake.nix
+++ flake.nix
@@ -10,8 +10,29 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        sundial = pkgs.rustPlatform.buildRustPackage {
+          pname = "sundial";
+          version = "1.0.0";
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          meta = with pkgs.lib; {
+            description = "A tool to control hyprsunset based on sunrise/sunset times";
+            homepage = "https://github.com/tcione/sundial";
+            license = licenses.mit;
+            maintainers = [ "tcione" ];
+          };
+        };
       in
       {
+        packages = {
+          default = sundial;
+          sundial = sundial;
+        };
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             rustc
